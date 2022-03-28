e463a59f4da741462242b1945a9ceb07974e197f
Miras.Ramazanov <mirasafa.lol@gmail.com>
2022-03-28 13:56:26 +0600

    uploaded some code

--- src/Miras.java
+++ src/Miras.java
@@ -1,3 +1,34 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Miras {
+    Scanner in = new Scanner(System.in); //enter directory
+            System.out.print("Enter directory:");
+    String dir = in.next();
+    File currentDir = new File(String.valueOf(dir)); // current directory
+    ArrayList<MyFile> list = new ArrayList<>(); //Collector list
+ //   displayDirectoryContents(list, currentDir);
+
+    Scanner in = new Scanner(System.in); //enter directory
+            System.out.print("Enter directory:");
+    String dir = in.next();
+    File currentDir = new File(String.valueOf(dir0)); // current directory
+    ArrayList<MyFile> list = new ArrayList<>(); //Collector list
+    //   displayDirectoryContents(list, currentDir);
+
+    Scanner in = new Scanner(System.in); //enter directory
+            System.out.print("Enter directory:");
+    String dir = in.next();
+    File currentDir = new File(String.valueOf(dir1)); // current directory
+    ArrayList<MyFile> list = new ArrayList<>(); //Collector list
+    //   displayDirectoryContents(list, currentDir);
 
 }
+
+123
+132
+
+1
+1
+1
