a6318eb8b566476d83cc3b6b6e99eb54ca2f4ef6
Kymbat Nusipkan <knussipkan@mail.ru>
2022-03-28 13:13:24 +0600

    Create a new class named City
    -create some variables
    -created a class constructor
    -created getters and setters

--- src/City.java
+++ src/City.java
@@ -1,2 +1,36 @@
 public class City {
+    private int id;
+    private String name;
+    private int population;
+    private String countryCode;
+    public  City(int id, String name, int population, String countryCode){
+        this.id = id;
+        this.name = name;
+        this.population = population;
+        this.countryCode = countryCode;
+    }
+    public void setId(int id){
+        id = id;
+    }
+    public  int getId(){
+        return id;
+    }
+    public  void setName(String n){
+        name = n;
+    }
+    public  String getName(){
+        return name;
+    }
+    public  void setPopulation(int p){
+        population = p;
+    }
+    public int getPopulation(){
+        return population;
+    }
+    public void setCountryCode(String c){
+        countryCode = c;
+    }
+    public  String getCountryCode(){
+        return  countryCode;
+    }
 }
