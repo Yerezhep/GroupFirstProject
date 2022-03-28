eb9a0955d6ac082320006d79a14dc32d1ab4e190
Sultan Yerezhep <yerezhepsultan11@gmail.com>
2022-03-28 13:27:25 +0600

    added new cars array list
    in method main add new cars array list:
    -add new elements
    -print cars list

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        //add Car Array list
+
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("Carolla", 200, CarModel.TOYOTA));
+        cars.add(new Car("B6", 180, CarModel.AUDI));
+
+        //print cars list
+        for (Car c: cars) {
+            System.out.println(c);
+        }
 
     }
 }
