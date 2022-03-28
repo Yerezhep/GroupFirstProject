00840917757839701c125bba9c673f45b8cc4cac
Kymbat Nusipkan <knussipkan@mail.ru>
2022-03-28 13:47:04 +0600

    Class city changed
    -added some variables
    -created a class construction
    -created getters and setters

--- src/City.java
+++ src/City.java
@@ -1,2 +1,29 @@
 public class City {
+    private int id;
+    private String name;
+    private int population;
+    public  City(int id, String name, int population){
+        this.id = id;
+        this.name = name;
+        this.population = population;
+    }
+    public  void setId(int id){
+        id = id;
+    }
+    public  int getId(){
+        return id;
+    }
+    public void setName(String n){
+        name = n;
+    }
+    public  String getName(){
+        return name;
+    }
+    public  void setPopulation(int p){
+        population = p;
+    }
+    public int getPopulation(){
+        return population;
+    }
+    
 }
